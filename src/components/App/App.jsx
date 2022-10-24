b5f0e1eff51cdf3b62498b88054b424431905346
VladyslavLata <vladlatahtc@gmail.com>
2022-10-24 18:06:54 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
 import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 import { Home } from 'pages/Home/Home';
-// import { MovieDetails } from 'pages/MovieDetails/MovieDetails';
-// import { Cast } from 'components/Cast/Cast';
-// import { Reviews } from 'components/Reviews/Reviews';
-// import { Movies } from 'pages/Movies/Movies';
 
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
